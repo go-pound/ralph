4d53f6fe4000bcd81bd809c89e146d0ab95507b5
Michael Wong <emporophobe@gmail.com>
2023-01-08 23:48:36 -0500

    Fix lambda_handler url verification

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,25 +6,19 @@ from app_mention_handler import AppMentionHandler
 
 
 def lambda_handler(event: json, context: json):
-    init_logger()
-    logging.info(f"Received event: {event} with context {context}")
-
-    body = json.loads(event['body']).get('event')
-    event_type = body["type"] if body else event['body']['type']  # url_verification events have a different structure
-
-    if event_type == "url_verification":
-        return url_verification(event['body'])
-    elif event_type == "app_mention":
-        return AppMentionHandler().respond(body)
-    else:
-        return {"statusCode": 400, "body": f"Unsupported event type {event_type}"}
-
-
-def init_logger():
     log_level = os.getenv("LOG_LEVEL", logging.WARN)
     logging.getLogger().setLevel(log_level)
 
+    logging.info(f"Received event: {event} with context {context}")
 
-def url_verification(body):
-    logging.info(f"Responding to challenge: {body}")
-    return body["challenge"]
+    body = json.loads(event['body'])
+    event = body.get('event')
+    event_type = event["type"] if event else body["type"]  # url_verification events have a different structure
+
+    if event_type == "url_verification":
+        logging.info(f"Responding to challenge: {body}")
+        return body["challenge"]
+    elif event_type == "app_mention":
+        return AppMentionHandler().respond(event)
+    else:
+        return {"statusCode": 400, "body": f"Unsupported event type {event_type}"}
